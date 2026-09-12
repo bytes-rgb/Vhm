1e060365b1e5016b903915f9f7d8e0aec9c1c48f
Neoi <110278900+bytes-rgb@users.noreply.github.com>
2026-09-12 15:02:49 -0400

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,7 @@
+Number = int(input("Enter the number: "))
+Number_2 = int(input("Enter the second Number: "))
+
+if Number_2 == 0:
+    print("Error: Division by zero is not allowed.")
+else:
+    print(f"Answer is {Number}/{Number_2}")
