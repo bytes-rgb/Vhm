d1735c072dd49b4032d791a2471f05005557a6e7
Neoi <110278900+bytes-rgb@users.noreply.github.com>
2026-09-14 18:00:15 -0400

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,18 @@
+First_Name = "Johnthan"
+Last_Name = "jerry"
+country = "USA"
+city = "Bowling Green"
+is_married = False
+skills = ["HTML","Python","C++"]
+person_info = {
+    "firstname": "Jam",
+    "lastname": "Yeagar",
+    "country": "USA",
+    "city": "Bowling Green"
+}
+
+print(f"Information about {First_Name} {Last_Name}:")
+if is_married:
+    print("Happy married life!")
+else:
+    print("You are not married yet.")
