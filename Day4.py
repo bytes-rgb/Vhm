f2a31e8e4d7c687d3b3cb23abd2c1a34f5d131da
Neoi <110278900+bytes-rgb@users.noreply.github.com>
2026-09-15 19:32:02 -0400

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,22 @@
+
+#Arithmetic operations:
+Total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+div = num_two / num_one
+remainder = num_two % num_one
+
+#printing values with label
+print("total:", Total)
+print("difference:", diff)
+print("product:", product)
+print("division:", div)
+print("remainder:", remainder)
+
+#calculating area of a circle
+
+width = 25
+radius = width / 2
+area_of_circle = 3.14 * radius ** 2
+print("Area of a circle:", area_of_circle)
+
