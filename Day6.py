f8f9df26b11b23bc4c11146b4b8282edc1f18788
Neoi <110278900+bytes-rgb@users.noreply.github.com>
2026-09-17 17:19:18 -0400

    Add files via upload

---
+++ Day6.py
@@ -0,0 +1,13 @@
+
+print("Vestagtables:", len("Vestagtables"))
+print("Apples:", len("Apples"))
+print("Tomato:", len("Tomato"))
+
+fruit = ["Vestagtables", "Apples", "Tomato"]
+Snacks = ["Chips", "cookies", "Candy"]
+
+print("Fruit items:", len(fruit))
+print("Snacks items:", len(Snacks))
+does_exist = "Banana" in fruit
+print("Does 'Banana' exist in fruit?", does_exist)
+
